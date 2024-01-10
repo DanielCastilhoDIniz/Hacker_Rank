8eab0e2a5c2e36fce9fb89751f431e1df285a6d1
Daniel Castilho <danielcastilhodiniz@gmail.com>
2024-01-09 22:56:57 -0300

    update

--- projecEuller/longestprimo.py
+++ projecEuller/longestprimo.py
@@ -29,28 +29,85 @@ Explanation 0
 Prime factors of  are , largest is .
 Prime factor of  is  itself, hence largest is .
 """
-n = 12
-# resto = n
-# while resto !=0:
-#     for i in range(2,n):
-#         if n % i == 0:
-#             quociente = n // i
+# n = 12
+# # resto = n
+# # while resto !=0:
+# #     for i in range(2,n):
+# #         if n % i == 0:
+# #             quociente = n // i
 
 
-print(24%2)
-print(24//2)
+# print(24%2)
+# print(24//2)
 
 
-def factor(n):
-    "Prime factors of n."
-    # factor(99) --> 3 3 11
-    # factor(1_000_000_000_000_007) --> 47 59 360620266859
-    # factor(1_000_000_000_000_403) --> 1000000000000403
-    for prime in sieve(math.isqrt(n) + 1):
-        while not n % prime:
-            yield prime
-            n //= prime
-            if n == 1:
-                return
-    if n > 1:
-        yield n
+# def factor(n):
+#     "Prime factors of n."
+#     # factor(99) --> 3 3 11
+#     # factor(1_000_000_000_000_007) --> 47 59 360620266859
+#     # factor(1_000_000_000_000_403) --> 1000000000000403
+#     for prime in sieve(math.isqrt(n) + 1):
+#         while not n % prime:
+#             yield prime
+#             n //= prime
+#             if n == 1:
+#                 return
+#     if n > 1:
+#         yield n
+import time
+start = time.time()
+primo = [1,2,3,5]
+for n in range(2, 30):
+    if n % 2 != 0 and n % 3 != 0 and n % 5 != 0:
+        primo.append(n)
+
+end = time.time()
+laeps= start - end
+print(primo)
+print(laeps)
+
+
+def crivo_eratostenes(N):
+    primos = []
+    numeros = [True] * (N + 1)
+    for i in range(2, int(N**0.5) + 1):
+        if numeros[i]:
+            primos.append(i)
+            for j in range(i*i, N+1, i):
+                numeros[j] = False
+    for i in range(int(N**0.5) + 1, N+1):
+        if numeros[i]:
+            primos.append(i)
+    return primos
+
+limite_superior = 30
+primos_ate_limite = crivo_eratostenes(limite_superior)
+print(primos_ate_limite)
+
+# com retorno do primos
+def prime_checker(number):
+  primo = []
+  for n in range(1, number+1):
+    if n % 2 != 0 and n % 3 != 0 and n % 5 != 0:
+        primo.append(n)
+  if number in primo:
+    print("It's a prime number.")
+   
+  else:
+    print("It's not a prime number.")
+    
+prime_checker(13)
+
+
+def prime_checker(number):
+  is_primo = True
+  for n in range(2, number):
+    if number % n == 0:
+        is_primo = False
+  if is_primo:
+    print("It's a prime number.")
+   
+  else:
+    print("It's not a prime number.")
+    
+prime_checker(13)
