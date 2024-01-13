84f47e64d5efb4c507839f55e5d50568deb82d16
Daniel Castilho <danielcastilhodiniz@gmail.com>
2024-01-13 01:55:18 -0300

    update

---
+++ maths/pollarcoodinates.py
@@ -0,0 +1,21 @@
+import cmath
+
+# z = 1 + 2j
+# # z = complex(input())
+# fase = cmath.phase(z)
+# r = abs(z)
+
+# print(r)
+# print(fase)
+
+# a = int(input())
+# b = int(input())
+# print(divmod(a//b))
+# print(divmod(a % b))
+# print(divmod(a, b))
+
+n = 5
+s = 0
+for i in range(1, n):
+
+    print((((10**i - 1)//9)*i))
